fc0c7941e55f8241be062b9505b66670b35d7551
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 09:14:21 +0100

    ho calcolato l'errore di tempo moltiplicando l'errore di troncamento per i valori: 100,60,60,10. Successivamente ho calcolato lo spazio moltiplicando l'errore di tempo per la velocità

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         double erroreTroncamento=0.00000095;
+         double erroreTempo;
+         double spazio;
+         int velocità = 1676;
+         erroreTempo = erroreTroncamento * 100 * 60 * 60 * 10;
+         Console.WriteLine("l'errore per il tempo è di {0}", erroreTempo);
+
+            spazio = erroreTempo * velocità;
+            Console.WriteLine("lo spazio è {0}", spazio);
+
+
+        }
+    }
+}
